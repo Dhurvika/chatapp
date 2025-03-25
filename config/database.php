cf416517b197ff328ec28eb8b5505ea35443b8d8
Dhruvika <dhruvikaranpara410@gmail.com>
2025-03-25 20:09:52 +0530

    FIXED mysql config

--- config/database.php
+++ config/database.php
@@ -41,26 +41,24 @@ return [
         //     'journal_mode' => null,
         //     'synchronous' => null,
         // ],
-
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DB_URL', ''),
+            'host' => env('DB_HOST', 'sql300.infinityfree.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'if0_38577163_whatsapp_db'),
+            'username' => env('DB_USERNAME', 'if0_38577163'),
+            'password' => env('DB_PASSWORD', 'aL29fvrpF8AM'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+
 
         'mariadb' => [
             'driver' => 'mariadb',
@@ -147,7 +145,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
